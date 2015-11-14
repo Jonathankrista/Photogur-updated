b6480af09d017d9a7956aa858e8f97c5eda7c266
Jon <jonathankrista@gmail.com>
2015-11-13 23:30:38 -0500

    Improved

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,16 @@
+class PicturesController < ApplicationController
+def index
+end
+end
+
+
+
+# class PicturesController < ApplicationController
+
+#   def new
+#   end
+
+#   def create
+#   	render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+#   end
+# end
